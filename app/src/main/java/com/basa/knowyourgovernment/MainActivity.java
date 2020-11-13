5961ed4cfed459a201ece7a4231b8822ec1b048f
basa <d.basaraba@gmail.com>
2020-11-13 17:53:52 -0600

    ask for device location

--- app/src/main/java/com/basa/knowyourgovernment/MainActivity.java
+++ app/src/main/java/com/basa/knowyourgovernment/MainActivity.java
@@ -1,11 +1,18 @@
 package com.basa.knowyourgovernment;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Intent;
-import android.net.Uri;
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -14,16 +21,42 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 
+import static android.content.pm.PackageManager.PERMISSION_GRANTED;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, View.OnLongClickListener {
 
+    private LocationManager locationManager;
+    private Criteria criteria;
     private final ArrayList<Official> officials = new ArrayList<>();
     private OfficialAdapter oAdapter = new OfficialAdapter(officials, this);
     private RecyclerView recyclerView;
+
+    private static final int MY_LOCATION_REQUEST_CODE_ID = 111;
+    private double lat;
+    private double lon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        criteria = new Criteria();
+        criteria.setPowerRequirement(Criteria.POWER_HIGH);
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
+        criteria.setSpeedRequired(false);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[] { Manifest.permission.ACCESS_FINE_LOCATION },
+                    MY_LOCATION_REQUEST_CODE_ID
+            );
+        }
+        else { setLocation(); }
 
         recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setAdapter(oAdapter);
@@ -46,8 +79,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         o3.setName("Tony the Tiger");
         o3.setParty("America");
         officials.add(o3);
+    }
 
-//        oAdapter.notifyDataSetChanged();
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode,
+            @NonNull String[] permissions,
+            @NonNull int[] grantResults) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == MY_LOCATION_REQUEST_CODE_ID) {
+            if (permissions[0].equals(Manifest.permission.ACCESS_FINE_LOCATION) &&
+                grantResults[0] == PERMISSION_GRANTED) { setLocation(); }
+        }
     }
 
     @Override
@@ -80,6 +125,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public boolean onLongClick(View v) {
         mToast("long click");
         return true;
+    }
+
+    @SuppressLint("MissingPermission")
+    private void setLocation() {
+        String bestProvider = locationManager.getBestProvider(criteria, true);
+        Location currentLocation = null;
+
+        if (bestProvider != null) { currentLocation = locationManager.getLastKnownLocation(bestProvider); }
+        if (currentLocation != null) {
+            lat = currentLocation.getLatitude();
+            lon = currentLocation.getLongitude();
+        }
     }
 
     private void mToast(String s) { Toast.makeText(this, s, Toast.LENGTH_SHORT).show(); }
